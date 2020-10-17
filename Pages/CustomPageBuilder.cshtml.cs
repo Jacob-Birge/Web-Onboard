8c322aa7363d198557d337416fac196317047b5a
Jacob-Birge <35941365+Jacob-Birge@users.noreply.github.com>
2020-10-17 16:33:09 -0400

    Custom Page With Quill

---
+++ Pages/CustomPageBuilder.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_Onboard.Pages
+{
+    public class CustomPageBuilderModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
