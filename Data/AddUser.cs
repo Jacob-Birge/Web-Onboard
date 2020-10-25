67ca817e280c0eea15372851889c4363afa76baf
jiangyedsb@gmail.com <Reyai19980124>
2020-10-25 15:45:49 -0400

    User Manager data model added

---
+++ Data/AddUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_Onboard.Data
+{
+    public class AddUser
+    {
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string role_id { get; set; }
+        public bool remember { get; set; }
+        public AddUser()
+        {
+            remember = true;
+        }
+    }
+}
